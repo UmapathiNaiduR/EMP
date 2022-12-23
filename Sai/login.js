e19d4bd71e3428890f79349b2afae1e361b8d92f
sai-krishna1997 <121084788+sai-krishna1997@users.noreply.github.com>
2022-12-23 13:15:13 +0530

    MYE 01 UPDATED FILE
    UPDATED MYE 1 PAGE

--- Sai/login.js
+++ Sai/login.js
@@ -10,13 +10,19 @@ return false;
 }
 else{
 attempt --;// Decrementing by one.
+// alert("send to mail;");
 alert("You have left "+attempt+" attempt;");
+// alert("send to mail;");
+// alert("You have left "+attempt+" attempt;");
 // Disabling fields after 3 attempts.
+
+
 if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
 return false;
 }
+
 }
 }
